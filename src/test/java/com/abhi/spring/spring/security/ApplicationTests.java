830fb476d6e1046d039f7d5a738d2b13bd88b0da
Abhinandan Kumar <abhkumar86@publicisgroupe.net>
2023-08-01 20:35:18 +0530

    First commit

---
+++ src/test/java/com/abhi/spring/spring/security/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.spring.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
